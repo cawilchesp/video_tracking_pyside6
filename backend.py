b02388c13699d9bf033e87bf9defcb361f6ca16a
Carlos Andrés Wilches Pérez <32644658+cawilchesp@users.noreply.github.com>
2023-10-23 09:36:30 -0500

    Update backend.py

--- backend.py
+++ backend.py
@@ -5,35 +5,8 @@ from PySide6.QtCore import Qt
 import cv2
 
 
-def convert_cv_qt(cv_img):
-        """Convert from an opencv image to QPixmap"""
-        rgb_image = cv2.cvtColor(cv_img, cv2.COLOR_BGR2RGB)
-        h, w, ch = rgb_image.shape
-        bytes_per_line = ch * w
-        convert_to_Qt_format = QtGui.QImage(rgb_image.data, w, h, bytes_per_line, QtGui.QImage.Format.Format_RGB888)
-        # p = convert_to_Qt_format.scaled(1280, 720, Qt.AspectRatioMode.KeepAspectRatio)
-        return QPixmap.fromImage(convert_to_Qt_format)
 
 
-def open_video(source_file: str):
-    cap = cv2.VideoCapture(source_file)
-    if not cap.isOpened():
-        print('Error opening video stream or file')
-        return 0
-
-    ret, frame = cap.read()
-
-    video_properties = {
-        'width': int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)),
-        'height': int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)),
-        'frame_count': int(cap.get(cv2.CAP_PROP_FRAME_COUNT)),
-        'fps': float(cap.get(cv2.CAP_PROP_FPS)),
-        'first_frame': convert_cv_qt(frame)
-    }
-
-    cap.release()
-
-    return video_properties
 
 
 def about_qt_dialog(parent, language:int):
