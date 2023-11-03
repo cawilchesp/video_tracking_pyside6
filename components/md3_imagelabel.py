78f00d5b4af73850a010fdd445d17c036bcf6b77
Carlos Andrés Wilches Pérez <32644658+cawilchesp@users.noreply.github.com>
2023-11-02 19:13:45 -0500

    Update md3_imagelabel.py

--- components/md3_imagelabel.py
+++ components/md3_imagelabel.py
@@ -1,25 +1,19 @@
 """
 PySide6 Video label component adapted to follow Material Design 3 guidelines
 
-
 """
-
-from PySide6 import QtWidgets
-
-from components.style_color import colors
+from PySide6.QtWidgets import QLabel, QFrame
 
 # -----------
 # Image Label
 # -----------
-class MD3ImageLabel(QtWidgets.QLabel):
+class MD3ImageLabel(QLabel):
     def __init__(self, parent, attributes: dict) -> None:
         """ Material Design 3 Component: Image Label
 
         Parameters
         ----------
         attributes: dict
-            name: str
-                Widget name
             position: tuple
                 Label position
                 (x, y) -> x, y: upper left corner
@@ -28,9 +22,6 @@ class MD3ImageLabel(QtWidgets.QLabel):
                 (w, h) -> w: width, h: height
             scaled_image: bool
                 Fit image to label size
-            theme: bool
-                App theme
-                True: Light theme, False: Dark theme
         
         Returns
         -------
@@ -41,30 +32,10 @@ class MD3ImageLabel(QtWidgets.QLabel):
         self.attributes = attributes
         self.parent = parent
 
-        self.name = attributes['name']
-        self.setObjectName(self.name)
-
         x, y = attributes['position'] if 'position' in attributes else (8,8)
         w, h = attributes['size'] if 'size' in attributes else (96, 96)
         self.setGeometry(x, y, w, h)
 
         self.setScaledContents(attributes['scaled_image'])
         
-        self.setFrameStyle(QtWidgets.QFrame.Shape.Box)
-
-        self.setThemeStyle(attributes['theme'])
-    
-
-    def setThemeStyle(self, theme: bool) -> None:
-        """ Apply theme style sheet to component """
-        
-        if self.parent.attributes['type'] == 'filled':
-            background_color = colors(theme, 'surface_tint')
-        elif self.parent.attributes['type'] == 'outlined':
-            background_color = colors(theme, 'background')
-        border_color = colors(theme, 'outline')
-        
-        self.setStyleSheet(f'QLabel#{self.name} {{ '
-                f'border: 1px solid {border_color};'
-                f'background-color: {background_color};'
-                f'}}' )
+        self.setFrameStyle(QFrame.Shape.Box)
