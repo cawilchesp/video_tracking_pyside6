5c15bda34d268b47be37a2dfe032a77b99d995e0
Carlos Andrés Wilches Pérez <32644658+cawilchesp@users.noreply.github.com>
2024-03-13 11:43:23 -0500

    Create colors.py

---
+++ themes/colors.py
@@ -0,0 +1,55 @@
+light_colors = {
+    '@background_full': (0, 0, 98),
+    '@background_card': (0, 0, 90),
+    '@background_widget': (0, 0, 93),
+    '@background_hover': (0, 0, 85),
+    '@border_handle': (0, 0, 30),
+    '@slider_page': (0, 0, 60),
+    '@background_disable': (0, 0, 71),
+    '@text_active': (0, 0, 13),
+    '@text_disable': (0, 0, 35),
+}
+
+dark_colors = {
+    '@background_full': (0, 0, 13),
+    '@background_card': (0, 0, 17),
+    '@background_widget': (0, 0, 20),
+    '@background_hover': (0, 0, 25),
+    '@border_handle': (0, 0, 30),
+    '@slider_page': (0, 0, 60),
+    '@background_disable': (0, 0, 16),
+    '@text_active': (0, 0, 98),
+    '@text_disable': (0, 0, 35),
+}
+
+theme_colors = {
+    'blue': {
+        '@theme_active': (200, 100, 50),
+        '@theme_hover': (200, 100, 40),
+    },
+    'red': {
+        '@theme_active': (0, 70, 50),
+        '@theme_hover': (0, 70, 40),
+    }
+}
+
+icons = {
+    True: {
+        '@chevron_down': 'chevron_down_black.png',
+        '@chevron_up': 'chevron_up_black.png',
+        '@chevron_left': 'chevron_left_black.png',
+        '@chevron_right': 'chevron_right_black.png',
+        '@done': 'done_white.png',
+        '@minus': 'minus_white.png',
+        '@calendar': 'calendar_black.png',
+    },
+    False: {
+        '@chevron_down': 'chevron_down_white.png',
+        '@chevron_up': 'chevron_up_white.png',
+        '@chevron_left': 'chevron_left_white.png',
+        '@chevron_right': 'chevron_right_white.png',
+        '@done': 'done_black.png',
+        '@minus': 'minus_black.png',
+        '@calendar': 'calendar_white.png',
+    }
+}
