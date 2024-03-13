eff3d603d79f20ebddc339eba2af6fe7936180ce
Carlos Andrés Wilches Pérez <32644658+cawilchesp@users.noreply.github.com>
2024-03-13 11:42:58 -0500

    Update about_app.py

--- dialogs/about_app.py
+++ dialogs/about_app.py
@@ -1,6 +1,7 @@
 from PySide6.QtWidgets import QDialog
 
 from dialogs.about_app_ui import AboutAppUI
+from themes.colors import dark_colors, light_colors, theme_colors, icons
 
 import yaml
 
@@ -22,13 +23,28 @@ class AboutApp(QDialog):
         self.theme_style = self.config['THEME_STYLE']
         self.theme_color = self.config['THEME_COLOR']
 
-        # ---
-        # GUI
-        # ---
+        # ----------------
+        # Generación de UI
+        # ----------------
         self.ui = AboutAppUI(self)
-        theme_file = f"themes/{self.theme_color}_light_theme.qss" if self.theme_style else f"themes/{self.theme_color}_dark_theme.qss"
-        with open(theme_file, "r") as theme_qss:
-            self.setStyleSheet(theme_qss.read())
+
+        # -----------
+        # Apply Theme
+        # -----------
+        with open('themes/style.qss', 'r') as qss_file:
+            style_qss = qss_file.read()
+        
+        style_colors = light_colors if self.theme_style else dark_colors
+        for color_name, color_value in style_colors.items():
+            style_qss = style_qss.replace(color_name, f"hsl({color_value[0]}, {color_value[1]}%, {color_value[2]}%)")
+        
+        for color_name, color_value in theme_colors[self.theme_color].items():
+            style_qss = style_qss.replace(color_name, f"hsl({color_value[0]}, {color_value[1]}%, {color_value[2]}%)")
+
+        for icon_name, icon_value in icons[self.theme_style].items():
+            style_qss = style_qss.replace(icon_name, icon_value)
+
+        self.setStyleSheet(style_qss)
 
 
     def on_ok_button_clicked(self):
